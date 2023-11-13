79c18c8a00149df1dbebd1e508012c746c7fd0eb
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-13 13:41:32 +0100

    prueba de carga en firestore

--- mi_app/main.py
+++ mi_app/main.py
@@ -1,6 +1,3 @@
-import json
-from google.cloud import firestore
-
 def funcion_gcp(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
@@ -8,26 +5,5 @@ def funcion_gcp(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
     file = event
+    print("hola mundo")
     print(f"Processing file: {file['name']}.")
-
-def write_to_firestore(request):
-    # Inicializa el cliente de Firestore
-    db = firestore.Client()
-
-    # Nombre de la colección y documento en Firestore
-    collection_name = "mi-coleccion"
-    document_name = "mi-documento"
-
-    # Datos a escribir en Firestore (puedes personalizar esto según tus necesidades)
-    data = {
-        'campo1': 'valor1',
-        'campo2': 'valor2',
-        'campo3': 'valor3'
-    }
-
-    # Escribe los datos en Firestore
-    doc_ref = db.collection(collection_name).document(document_name)
-    doc_ref.set(data)
-
-    return 'Datos escritos en Firestore correctamente.'
-
