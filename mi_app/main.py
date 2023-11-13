3ca54c8d30adb44b203e004bd87bb8b51284c730
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-13 12:31:00 +0100

    tercera prueba

---
+++ mi_app/main.py
@@ -0,0 +1,8 @@
+def funcion_gcp(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
