735ee570c29e391ef1d61cebe7d5f6cd0e932149
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-13 13:54:15 +0100

    prueba de carga en firestore

--- mi_app/main.py
+++ mi_app/main.py
@@ -1,3 +1,6 @@
+import json
+from google.cloud import storage
+
 def funcion_gcp(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
@@ -6,4 +9,25 @@ def funcion_gcp(event, context):
     """
     file = event
     print("hola mundo")
-    print(f"Processing file: {file['name']}.")
+
+def print_json_content(event, context):
+    # Obtén información sobre el archivo desde el evento de Cloud Storage
+    bucket_name = event['bucket']
+    file_name = event['name']
+
+    # Inicializa el cliente de Cloud Storage y descarga el archivo JSON
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    json_data = blob.download_as_text()
+
+    # Parsea el JSON
+    data = json.loads(json_data)
+
+    # Imprime los datos en los registros de la función
+    print(f'Datos del archivo {bucket_name}/{file_name}:')
+    print(json.dumps(data, indent=2))
+
+    # Puedes agregar más lógica según tus necesidades
+
+    return 'Proceso completado'
