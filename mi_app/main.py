824ee2f60519ffcdfed62ce386e0482a4807e762
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-13 12:54:12 +0100

    prueba de carga en firestore

--- mi_app/main.py
+++ mi_app/main.py
@@ -1,8 +1,34 @@
-def funcion_gcp(event, context):
-    """Triggered by a change to a Cloud Storage bucket.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-    file = event
-    print(f"Processing file: {file['name']}.")
+import json
+from google.cloud import storage, firestore
+
+# Creando instancias de Cloud Storage y Cloud Firestore
+storage_client = storage.Client()
+firestore_client = firestore.Client()
+
+def gcs_to_firestore(event, context):
+    # Obtiene el nombre del bucket y la clave del archivo JSON del evento de GCS
+    bucket = event['bucket']
+    file_name = event['name']
+    print(bucket)
+    print(file_name)
+
+    # Descarga el archivo JSON desde Cloud Storage
+    bucket = storage_client.get_bucket(bucket)
+    blob = bucket.blob(file_name)
+    json_data = blob.download_as_text()
+
+    # Parsea el JSON
+    data = json.loads(json_data)
+
+    # Inserta los datos en Cloud Firestore
+    db = firestore.Client()
+    doc_ref = db.collection('bbdd-ejercicio-gcp').document()
+    doc_ref.set({
+        'ID': data['ID'],
+        'Nombre': data['Nombre'],
+        'Correo electrónico': data['Correo electrónico'],
+        'Fecha de registro': data['Fecha de registro']
+    })
+
+    return 'Proceso completado'
+
