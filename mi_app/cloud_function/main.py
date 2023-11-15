681d1fe4cef02493859d6ee1bfd5391de960d381
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-15 17:24:16 +0100

    tercera prueba

--- mi_app/cloud_function/main.py
+++ mi_app/cloud_function/main.py
@@ -6,7 +6,7 @@ storage_client = storage.Client()
 firestore_client = firestore.Client()
 
 
-def hello_gcs(event, context):
+def function_gcs(event, context):
     # Descarga el archivo desde Cloud Storage
     blob = storage_client.bucket(event["bucket"]).get_blob(event["name"])
     content = blob.download_as_text()
